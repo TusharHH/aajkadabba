b88cd3b723da146810977e57a98d7806069b5bde
GIRISHGHUGARE <girishghugare19@gmail.com>
2024-10-10 03:35:25 +0530

    HomePage Readjusted

--- client/src/components/HomePageCart.js
+++ client/src/components/HomePageCart.js
@@ -7,14 +7,14 @@ const HomePageCart = () => {
                 <div className='row align-content-center'>
                     <div className='col-lg-4 col-md-4 col-sm-12 px-4'>
                         <div className='row d-flex justify-content-center'>
-                            <img src='./images/Thali.png'></img>
+                            <img src='./images/ThaliCart2.png'></img>
                         </div>
                         <div className='row mt-5'>
-                            <p className="lead text-center fw-bold">
+                            <p className="lead text-center fw-bold" style={{fontSize:"24px"}}>
                                 Paneer Bhuna<br />
                                 Masala
                             </p>
-                            <button className='border-0 fw-bold bg-white' style={{ color: "#DC4412" }}>Order Now &gt;</button>
+                            <button className='border-0 fw-bold bg-white' style={{ fontSize:"24px" ,color: "#FC8A06" }}>Order Now &gt;</button>
                         </div>
                     </div>
                     <div className='col-lg-4 col-md-4 col-sm-12 px-4'>
@@ -22,23 +22,23 @@ const HomePageCart = () => {
                             <img src='./images/Thali.png'></img>
                         </div>
                         <div className='row mt-5'>
-                            <p className="lead text-center fw-bold">
+                            <p className="lead text-center fw-bold" style={{fontSize:"24px"}}>
                                 Paneer Bhuna<br />
                                 Masala
                             </p>
-                            <button className='border-0 fw-bold bg-white' style={{ color: "#DC4412" }}>Order Now &gt;</button>
+                            <button className='border-0 fw-bold bg-white' style={{ fontSize:"24px" ,color: "#FC8A06" }}>Order Now &gt;</button>
                         </div>
                     </div>
                     <div className='col-lg-4 col-md-4 col-sm-12 px-4'>
                         <div className='row d-flex justify-content-center'>
-                            <img src='./images/Thali.png'></img>
+                            <img src='./images/ThaliCart2.png'></img>
                         </div>
                         <div className='row mt-5'>
-                            <p className="lead text-center fw-bold">
+                            <p className="lead text-center fw-bold" style={{fontSize:"24px"}}>
                                 Paneer Bhuna<br />
                                 Masala
                             </p>
-                            <button className='border-0 fw-bold bg-white' style={{ color: "#DC4412" }}>Order Now &gt;</button>
+                            <button className='border-0 fw-bold bg-white' style={{ fontSize:"24px" ,color: "#FC8A06" }}>Order Now &gt;</button>
                         </div>
                     </div>
                 </div>
