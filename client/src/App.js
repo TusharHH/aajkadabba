68a7b88edf2903010cbe0d3bccf01b1aaef6355c
GIRISHGHUGARE <girishghugare19@gmail.com>
2024-10-09 15:03:38 +0530

    Phase1: Home Page

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <>
       <DabbaRoutes/>
-      <h1>Girish</h1>
     </>
   );
 }
---
+++ client/src/components/HomePageCart.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomePageCart = () => {
+  return (
+    <>HomePageCart</>
+  )
+}
+
+export default HomePageCart
