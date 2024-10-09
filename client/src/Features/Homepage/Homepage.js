bfad178a4f60436cd8401535877b411f433116fe
akshayshadow321 <163079916+akshayshadow321@users.noreply.github.com>
2024-10-09 17:43:10 +0530

    login form added!!

--- client/src/Features/Homepage/Homepage.js
+++ client/src/Features/Homepage/Homepage.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import HomePageCart from '../../components/HomePageCart'
+import Navbar from '../../components/Navbar'
 
 const Homepage = () => {
     return (
         <>
-            <div className="d-flex justify-content-center align-items-center bg-white shadow-lg rounded-bottom-circle" style={{height:"80vh"}}>
-                <div className='container mt-5'>
+            <Navbar />
+            <div className="d-flex justify-content-center align-items-center bg-white shadow-lg rounded-bottom-circle  d-none d-sm-block" style={{height:"80vh"}}>
+                <div className='container '>
                     <div className='row'>
-                        <div className='col-lg-6 col-md-6 col-sm-12 align-content-lg-center'>
+                        <div className='col-lg-6 col-md-6 col-sm-12 align-content-lg-center  mt-5'>
                             <h1 className="display-5 fw-bold">Experience food<br /> Delivery like no other</h1>
                             <p className="lead mb-4">
                                 We deliver the food of your choice wherever, whenever.
@@ -18,7 +20,7 @@ const Homepage = () => {
                                 <button type="button" className="btn btn-custom btn-lg px-4 gap-3">Get Started</button>
                             </div>
                         </div>
-                        <div className='col-lg-6 col-md-6 col-sm-12 d-flex justify-content-center'>
+                        <div className='col-lg-6 col-md-6 col-sm-12 d-flex justify-content-center  mt-5'>
                             <img src='./images/Thali.png'></img>
                         </div>
                     </div>
--- client/src/Routes/DabbaRoutes.js
+++ client/src/Routes/DabbaRoutes.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import  { Route,Routes } from 'react-router-dom'
 import Homepage from '../Features/Homepage/Homepage';
+import HomePageCart from '../components/HomePageCart';
+import RegisterAsHomeMaker from '../Features/Authentication/RegisterAsHomeMaker/RegisterAsHomemaker';
 
 function DabbaRoutes (){
     return (
         <>
             <Routes>
                 <Route path='/' element={<Homepage/>}></Route>
+                <Route path='/login' element={<RegisterAsHomeMaker/>} ></Route>
             </Routes>
         </>
     )
