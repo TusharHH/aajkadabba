a43754aed13204616ab212af688614b99055035d
Tushar <tushar.hasule@somaiya.edu>
2024-10-09 17:14:26 +0530

    added api call for customers !!

--- server/server.js
+++ server/server.js
@@ -8,13 +8,15 @@ const app = express();
 const connectDB = require('./connection.js');
 
 const HomemakerRoutes = require('./routes/Homemaker.routes.js');
+const CustomerRoutes = require('./routes/Customer.routes.js');
 
 connectDB();
 
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1/homemakers', HomemakerRoutes);
+app.use('/api/v1/customers', CustomerRoutes);
 
 const PORT = process.env.PORT || 5000;
 
